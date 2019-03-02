a67c3da511d91706f837009412bd02d8dcce1f39
Aishwariy Baheti <aishwariybaheti@gmail.com>
2019-03-02 22:56:57 +0530

    New solution with rails 5.2.2

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
-Portfolio Management Website for CryptoCurrency
-Project taken up as a part of a course.
-02.03.2019
+# README
+
+This README would normally document whatever steps are necessary to get the
+application up and running.
+
+Things you may want to cover:
+
+* Ruby version
+
+* System dependencies
+
+* Configuration
+
+* Database creation
+
+* Database initialization
+
+* How to run the test suite
+
+* Services (job queues, cache servers, search engines, etc.)
+
+* Deployment instructions
+
+* ...
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-CryptFolio::Application.config.secret_token = '5f53e6213d235daf97b1c5646e46eee9a738bac7169a607c2aa91c7e61632723386d194251a173424e686c4d16967d9be65c191de46cc6643425ca157d2c6301'
